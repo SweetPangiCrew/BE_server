5569d657387753b9721bf437aa52e9d8e7bb3dea
wwonnn <dnjsgml0508@naver.com>
2023-12-20 15:51:52 +0900

    scratch 집회 참석 변수 추가

--- backend_server/npc_server/utils.py
+++ backend_server/npc_server/utils.py
@@ -1,5 +1,5 @@
 # Copy and paste your OpenAI API Key
-openai_api_key = "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"
+openai_api_key = "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"
 # Put your name
 key_owner = "WH"
 
