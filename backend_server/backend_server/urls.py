8ff7786d21a5be651f4b8599c40fa3c9d58ff4d5
dasoya <66422476+dasoya@users.noreply.github.com>
2024-04-12 19:26:49 +0900

    rm restauth

--- backend_server/backend_server/urls.py
+++ backend_server/backend_server/urls.py
@@ -41,8 +41,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('chat/',include("chat.urls")),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls'))
+   # path('rest-auth/', include('rest_auth.urls')),
+    #path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
 
 
